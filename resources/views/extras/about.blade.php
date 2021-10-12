92cb2d7dc74bec981f87a6246ec572660eb968a1
Emiliano <emigb2409@gmail.com>
2021-10-11 22:07:14 -0500

    New Views

---
+++ resources/views/extras/about.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <div class="text-justify text-gray-500">
+        <section class="mb-8 bg-cover" style="background-image: url({{asset('img/about/esfera.jpg')}})">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
+                <div class="w-full md:w-3/4 lg:w-1/2">
+                    <h1 class="text-white font-bold text-4xl">Bienvenid@s a HyperCode</h1>
+
+                    <p class="text-white text-lg mt-2 mb-4">Buscamos abrirte nuevos caminos y que aprendas algo nuevo dia a dia con nuestra ayuda</p>
+
+                </div>
+            </div>
+        </section>
+
+        <div class="w-full md:w-3/4 lg:w-1/2">
+            <h2 class="text-3xl md:pl-20 text-gray-700">¿Quiénes somos?</h2>
+            <p class="md:pl-20 text-lg mb-4">Somos HyperCode, lugar de aprendizaje</p>
+
+            <h2 class="text-3xl md:pl-20 text-gray-700">¿A qué aspiramos?</h2>
+            <p class="md:pl-20 text-lg mb-4">Buscamos ser una empresa la cual te ayude en tu caminó al exito y te acompañe cuando mas lo necesites</p>
+        </div>
+
+    </div>
+</x-app-layout>
---
+++ resources/views/extras/partner.blade.php
@@ -0,0 +1,83 @@
+<x-app-layout>
+    
+    <div class="text-center text-gray-500 my-4">
+        <h1 class="uppercase text-3xl">Partners</h1>
+        <p>Empresas, comunidades, organizaciones que confían en HyperCode</p>
+
+        <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid sm:grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-xl uppercase">Surf Entertainment</h1>
+                    <article>
+                        <figure>
+                            <img class="rounded-xl my-2 h-36 w-full object-cover" src="{{asset('img/partners/default.jpg')}}" alt="Imagen Default">
+                        </figure>
+                    </article>
+                    <p class="mb-4">Casa de entretenimiento, peliculas, juegos y más</p>
+
+                    <a target="_blank" href="https://discord.gg/hfr2kfbnfm" class="btn btn-primary">Más Información</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-xl uppercase">Leno</h1>
+                    <article>
+                        <figure>
+                            <img class="rounded-xl my-2 h-36 w-full object-cover" src="{{asset('img/partners/leno.png')}}" alt="Leno_Banner">
+                        </figure>
+                    </article>
+                    <p class="mb-4">Seguramente conozcas Steam, Epic Games, Battle.net, Origin, Uplay, GOG... Pero ¿Por qué no creamos nuestro propio launcher para videojuegos por medio de la comunidad?</p>
+
+                    <a target="_blank" href="https://lenobeta.xyz" class="btn btn-primary">Más Información</a>
+                
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </section>
+
+        <h2 class="uppercase text-2xl mt-4">Discord</h2>
+        <p>Bots y comunidades que nos apoyan</p>
+
+        <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid sm:grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-xl uppercase">Umbrella</h1>
+                    <article>
+                        <figure>
+                            <img class="rounded-xl my-2 h-36 w-full object-cover" src="https://umbrellabot.xyz/images/host/banner.gif" alt="Umbrella_Banner">
+                        </figure>
+                    </article>
+                    <p class="mb-4">Bot de discord multifunciones; Moderación, Seguridad, Diversióm y más</p>
+
+                    <a target="_blank" href="https://umbrellabot.xyz" class="btn btn-primary">Más Información</a>
+                </div>
+            </div> 
+
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </section>
+
+        <section class="mt-4">
+            <h2 class="text-2xl">¿Buscas ser socio de HyperCode?</h2>
+            <p class="mb-4">Estamos encantados por recibirte con nosotros, para mas informacion entra a nuestro discord</p>
+            <a href="https://discord.gg/AqBXHU4QZd" class="btn btn-danger" target="_blank">Discord</a>
+        </section>
+
+    </div>
+
+</x-app-layout>
