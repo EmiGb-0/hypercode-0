9f31b01e25d3ad8e858414e9aefd1d89f7b01bd2
Emiliano <emigb2409@gmail.com>
2021-10-17 14:38:24 -0500

    Implementacion del Favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="shortcut icon" href="{{asset('img/logos/favicon.ico')}}" type="image/x-icon">
+        <link rel="icon" href="{{asset('img/logos/favicon.ico')}}" type="image/x-icon">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
