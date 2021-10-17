3c7e08d913f0c85a5e9f640ee6b0cf651478698b
Emiliano <emigb2409@gmail.com>
2021-10-17 14:52:50 -0500

    Bugs Fixed

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,3 +1,1 @@
-<figure>
-  <img src="{{asset('img/logos/hyper.png')}}" alt="Logo_HyperCode" class="h-12 w-12">
-</figure>
+<img src="{{asset('img/logos/Hyper.png')}}" alt="Logo_HyperCode" class="h-12 w-12">
