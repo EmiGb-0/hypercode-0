0dc40fb18be89b13fec934cdfa824c0813cab075
Emiliano <emigb2409@gmail.com>
2021-10-28 11:31:25 -0500

    Se agrego un crud

---
+++ app/Http/Controllers/Admin/PartnerController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Partner;
+
+class PartnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $partners = Partner::all();
+
+        return view('admin.partners.index', compact('partners'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.partners.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|unique:partners',
+            'subtitle' => 'required|unique:partners',
+            'image' => 'required',
+            'link' => 'required'
+        ]);
+
+        $partner = Partner::create($request->all());
+
+        return redirect()->route('admin.partners.edit', $partner)->with('info', 'El partner se creo con éxito');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Partner $partner)
+    {
+        return view('admin.partners.show', compact('partner'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Partner $partner)
+    {
+        return view('admin.partners.edit', compact('partner'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Partner $partner)
+    {
+        $request->validate([
+            'title' => 'required|unique:partners,title,' . $partner->id,
+            'subtitle' => 'required|unique:partners,subtitle,' . $partner->id,
+            'image' => 'required',
+            'link' => 'required'
+        ]);
+
+        $partner->update($request->all());
+        return redirect()->route('admin.partners.edit', $partner)->with('info', 'El partner se actualizo con éxito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Partner $partner)
+    {
+        $partner->delete();
+        return redirect()->route('admin.partners.index')->with('info', 'El partner se elimino con éxito');
+    }
+}
