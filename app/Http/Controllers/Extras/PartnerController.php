1fb9ad70e974d12d2ff2c7d6061100e45ae9188a
Emiliano <emigb2409@gmail.com>
2021-10-28 11:32:09 -0500

    Se paso la informacion de la DB hacia partner

--- app/Http/Controllers/Extras/PartnerController.php
+++ app/Http/Controllers/Extras/PartnerController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Extras;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Partner;
 
 class PartnerController extends Controller
 {
     public function index()
     {
-        return view('extras.partner');
+        $partners = Partner::all();
+
+        return view('extras.partner', compact('partners'));
     }
 }
